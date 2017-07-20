654fc93ab77ac06dd0512c4cc726a1132a1d8f33
amanzeekverma <avd.zeek@gmail.com>
2017-07-20 14:24:01 -0400

    init

---
+++ ZeeKMouse.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.awt.Robot;
+
+public class ZeeKMouse {
+
+    public static final int FIVE_SECONDS = 5000;
+    public static final int MAX_Y = 400;
+    public static final int MAX_X = 400;
+
+    public static void main(String... args) throws Exception {
+        int time = Integer.parseInt(args[0]);
+        Robot robot = new Robot();
+        Random random = new Random();
+        while (true) {
+            robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
+            Thread.sleep(time);
+        }
+    }
+}
+
